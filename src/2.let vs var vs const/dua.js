0e402aac4a38031149c8c84a7ab8966bb1d3a0e6
reyhanawaludin <rrqramadhann2@gmail.com>
2019-03-27 10:20:53 +0700

    hard work to pray work

---
+++ src/2.let vs var vs const/dua.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export default class dua extends Component {
+  render() {
+    //var -> function
+    //let -> block
+    //const -> block
+    function rehhann(){
+      for(var i = 0; i < 9; i++){
+        console.log(i);
+      }
+      console.log(i);
+    }
+    rehhann();
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
